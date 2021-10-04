50b9c62ed1341e82de2fa089982afba1f9c83914
Ivan Khoda <ivankhoda@gmail.com>
2021-10-04 22:31:52 +0300

    Fix database initial file

--- database/database.js
+++ database/database.js
@@ -1,14 +1,14 @@
-const mysql = require("mysql");
+const mysql = require('mysql');
 
-const databaseConfig = require("../config/db.config.js");
-//Set up connection with database
+const databaseConfig = require('../config/db.config');
+// Set up connection with database
 
 const databaseConnection = mysql.createConnection(databaseConfig.databaseOptions);
-databaseConnection.connect(function (err) {
+databaseConnection.connect((err) => {
   if (err) {
-    return console.error("Ошибка: " + err);
+    return console.error(`Ошибка: ${err}`);
   }
-  console.log("Подключение к серверу MySQL успешно установлено");
+  console.log('Подключение к серверу MySQL успешно установлено');
 });
 
 module.exports = {
